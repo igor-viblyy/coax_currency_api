c94ea47281ae5bf3bb88749bbb9adeeec897fccb
Igor Viblyy <igor.viblyy@gmail.com>
2018-10-05 15:16:51 +0300

    fixed some error

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,13 +2,11 @@ class CurrenciesController < ApplicationController
   before_action :set_user, only: %i[create destroy]
   before_action :set_currency, only: %i[destroy]
 
-
   def create
     @currency = @user.currencies.build(from: params[:from], to: params[:to], user_id: params[:user])
 
     if @currency.save!
       render json: @currency, status: :created
-      # CurrencyCheckJob.perform_now(@currency.id)
     else
       render json: @currency.errors, status: :unprocessable_entity
     end
